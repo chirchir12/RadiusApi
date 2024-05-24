b0757b5277b6053dec8bdd3278918b13207b6eb8
chirchir <chirchir7370@gmail.com>
2024-05-24 14:43:12 +0300

    radreply

---
+++ priv/repo/migrations/20240524113409_create_radreply.exs
@@ -0,0 +1,16 @@
+defmodule RadiusApi.Repo.Migrations.CreateRadreply do
+  use Ecto.Migration
+
+  def change do
+    create table(:radreply) do
+      add :UserName, :text, null: false, default: ""
+      add :Attribute, :text, null: false, default: ""
+      add :op, :string, null: false, default: "=="
+      add :Value, :text, null: false, default: ""
+
+      timestamps(type: :utc_datetime)
+    end
+    create index(:radreply, [:UserName,:Attribute], name: :radreply_UserName)
+
+  end
+end
