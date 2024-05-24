976c61e743ceb7e1262535f2519145f026665a33
chirchir <chirchir7370@gmail.com>
2024-05-24 07:56:03 +0300

    ecto networks

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule RadiusApi.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:ecto_network, "~> 1.5.0"}
     ]
   end
 
