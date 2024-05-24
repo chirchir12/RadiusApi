160bc520077d81453c48c9319e200851c618fe0f
chirchir <chirchir7370@gmail.com>
2024-05-24 14:23:53 +0300

    radgroupcheck

---
+++ lib/radiusApi/policies/radgroupcheck.ex
@@ -0,0 +1,37 @@
+defmodule RadiusApi.Policies.Radgroupcheck do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @permitted [
+    :group_name,
+    :attribute,
+    :op,
+    :value
+  ]
+
+  @required [
+    :group_name,
+    :attribute,
+    :op,
+    :value
+  ]
+
+  schema "radgroupcheck" do
+    field :group_name, :string, source: :GroupName
+    field :attribute, :string, source: :Attribute
+    field :op, :string
+    field :value, :string, source: :Value
+    timestamps(type: :utc_datetime)
+  end
+
+  def changeset(t, attr \\ %{}) do
+    t
+    |> cast(attr, @permitted)
+    |> validate_required(@required)
+  end
+
+
+
+
+
+end
---
+++ priv/repo/migrations/20240524105838_create_radgroupcheck.exs
@@ -0,0 +1,15 @@
+defmodule RadiusApi.Repo.Migrations.CreateRadgroupcheck do
+  use Ecto.Migration
+
+  def change do
+    create table(:radgroupcheck) do
+      add :GroupName, :string, null: false, default: ""
+      add :Attribute, :string, null: false, default: ""
+      add :op, :string, null: false, default: "=="
+      add :Value, :string, null: false, default: ""
+      timestamps(type: :utc_datetime)
+    end
+
+    create index(:radgroupcheck, [:GroupName,:Attribute], name: :radgroupcheck_GroupName)
+  end
+end
